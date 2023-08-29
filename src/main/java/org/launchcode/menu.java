a698d3329e703fa112be32b711356e8450478c81
Abdu9991 <abduadam688@gmail.com>
2023-08-28 23:30:58 -0400

    finishing menu class and menuItem class

--- src/main/java/org/launchcode/menu.java
+++ src/main/java/org/launchcode/menu.java
@@ -1,2 +1,25 @@
-package org.launchcode;public class menu {
+package org.launchcode;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class menu {
+    private List<menuItem> menuItems;
+    private Date lastUpdate;
+    public menu(){
+       menuItems = new ArrayList<>();
+        lastUpdate = new Date(); // Initialize with the current date.
+
+    }
+    public  void addMenuItem(menuItem item){
+        menuItems.add(item);
+        lastUpdate = new Date();// update lastupdated when a new item is added
+    }
+    public List<menuItem> getMenuItems(){
+        return menuItems;
+    }
+    public Date getLastUpdate(){
+        return lastUpdate;
+    }
 }
--- src/main/java/org/launchcode/menuItem.java
+++ src/main/java/org/launchcode/menuItem.java
@@ -1,2 +1,38 @@
-package org.launchcode;public class menuItem {
+package org.launchcode;
+
+public class menuItem {
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+    private boolean isNew;
+    public menuItem(String name, double price, String description,String category, boolean isNew){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+public boolean isNew(){
+        return isNew;
+}
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
 }
